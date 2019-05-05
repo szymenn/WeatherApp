f1fd41796a35a0df27663bcc72a19ba782e2fc7d
szymenn <ilikedzimi@gmail.com>
2019-05-05 13:54:58 +0200

    Modify Startup.cs

--- WeatherApi/Startup.cs
+++ WeatherApi/Startup.cs
@@ -75,11 +75,11 @@ namespace WeatherApi
             }
             else
             {
+                app.UseCustomExceptionHandler();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseCustomExceptionHandler();
 
             app.UseHttpsRedirection();
 
