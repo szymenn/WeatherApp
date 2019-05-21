b8ea0b372b895c398ad96c346f92b3395dacef50
szymenn <ilikedzimi@gmail.com>
2019-05-21 18:21:26 +0200

    Move application code to src directory

--- IdentityServerService/Helpers/Constants.cs
+++
@@ -1,8 +0,0 @@
-namespace IdentityServerService.Helpers
-{
-    public class Constants
-    {
-        public const string GoogleClientId = "943653824845-sr18pu5ekjt2tfltjndi9vv1solbu4a4.apps.googleusercontent.com";
-        public const string GoogleClientSecret = "06bqft-Lf_IQ2s7bHsM9fJUC";
-    }
-}
---
+++ src/IdentityServerService/Configuration/Config.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System.Collections.Generic;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace IdentityServerService.Configuration
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(), 
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new[]
+            {
+                new ApiResource("WeatherApi", "My Weather API", new[]
+                {
+                    JwtClaimTypes.Id
+                } ), 
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "WeatherApi",
+                    ClientName = "WeatherApi Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = {"WeatherApi"}
+                },
+                new Client
+                {
+                    ClientId = "WebClient",
+                    ClientName = "WebClient",
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = {"https://localhost:5003/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:5003/signout-callback-oidc"},
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "WeatherApi"
+                    },
+
+                    AllowOfflineAccess = true,
+                    UpdateAccessTokenClaimsOnRefresh = true
+                }
+            };
+        }
+    }
+}
